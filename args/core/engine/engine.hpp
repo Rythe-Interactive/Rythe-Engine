d3045fb3c6e4000894157820ae99b8a9902be429
GlynLeine <g.m.leine@gmail.com>
2020-07-11 16:18:06 +0200

    engine modules and entry point define
    [Add] define ARGS_ENTRY to be defined in application upon first include of core.hpp
    [Add] engine
    [Add] module
    [Add] sfinae.hpp
    [Add] primitives.hpp

--- args/core/engine/engine.hpp
+++ args/core/engine/engine.hpp
@@ -1,9 +1,30 @@
 #pragma once
 #include <core/platform/platform.hpp>
+#include <core/engine/module.hpp>
+#include <core/types/primitives.hpp>
+#include <core/types/sfinae.hpp>
+
+#include <map>
+#include <vector>
 
 namespace args::core
 {
 	class ARGS_API Engine
 	{
+	private:
+		std::map<priority_type, std::vector<Module*>> modules = {};
+
+	public:
+		Engine() {};
+
+		template<class ModuleType, inherits_from<ModuleType, Module> = 0>
+		void reportModule()
+		{
+			auto* module = new ModuleType();
+			priority_type priority = module->priority();
+			modules[priority].push_back(module);
+		}
+
+		void init();		
 	};
 }
---
+++ args/core/types/primitives.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace args::core
+{
+	using priority_type = signed char;
+}
