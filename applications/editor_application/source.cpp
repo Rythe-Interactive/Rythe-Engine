0ebaf2a22172db5aca7fe57a174a4b961d86bfef
Algo-ryth-mix <mail.baier.raphael@gmail.com>
2020-07-12 15:43:30 +0200

    Revert "Revert "Merge pull request #10 from Args-Engine/feature/basic_file_operations""
    This reverts commit 56900b6b99f016ca22c817f45633d6f7aceba658.

--- applications/editor_application/source.cpp
+++ applications/editor_application/source.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <core/core.hpp>
+#include <editor/editor.hpp>
+
+void __cdecl reportEditorModules(args::editor::Editor* editor)
+{
+
+}
 
 void __cdecl reportModules(args::core::Engine* engine)
 {
---
+++ args/core/common/common.hpp
@@ -0,0 +1,3 @@
+#pragma once
+#include <cassert>
+#define assert_msg(msg,expr) assert(((void)(msg),(expr)))
