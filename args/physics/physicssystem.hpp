6fb1bf167e71e107fbba498d8a46e17c5226b326
DeveloperTheGreat <blue.person16@gmail.com>
2020-09-09 09:39:57 +0200

    Revert "commit so i can revert"
    This reverts commit ee6a49a1fbe71ba4ce58517886f509e4227bd33b.

---
+++ args/physics/physicssystem.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#ifndef ARGS_IMPORT
+#define ARGS_IMPORT
+#include <core/core.hpp>
+#include <core/platform/args_library.hpp>
+#else
+#include <core/core.hpp>
+#endif // !ARGS_IMPORT
+
+
+
+namespace args::physics
+{
+    class PhysicsSystem final : public System<PhysicsSystem>
+    {
+    public:
+
+        virtual void setup()
+        {
+            createProcess<&PhysicsSystem::fixedUpdate>("Physics", m_timeStep);
+        }
+
+        void fixedUpdate(time::time_span<fast_time> deltaTime)
+        {
+            runPhysicsPipeline();
+
+            integrateRigidbodies(deltaTime);
+
+        }
+
+    private:
+
+
+        const float m_timeStep = 0.02f;
+
+        /** @brief Performs the entire physics pipeline (
+         * Broadphase Collision Detection, Narrowphase Collision Detection, and the Collision Resolution)
+        */
+        void runPhysicsPipeline()
+        {
+            //Broadphase Optimization
+
+            //Narrophase 
+
+            //Collision Resolution
+
+        }
+
+        /** @brief gets all the entities with a rigidbody component and calls the integrate function on them
+        */
+        void integrateRigidbodies(float deltaTime)
+        {
+
+        }
+    };
+}
