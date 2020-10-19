c2920605cb2437d712f63bfbc74b29b0c58dd4d4
GlynLeine <g.m.leine@gmail.com>
2020-10-19 17:30:45 +0200

    return GLAPI to its original glory

--- legion/engine/application/context/detail/glad/glad.h
+++ legion/engine/application/context/detail/glad/glad.h
@@ -55,7 +55,29 @@ struct gladGLversionStruct {
 typedef void* (* GLADloadproc)(const char *name);
 
 #ifndef GLAPI
-#define GLAPI  __declspec(dllexport) extern
+# if defined(GLAD_GLAPI_EXPORT)
+#  if defined(_WIN32) || defined(__CYGWIN__)
+#   if defined(GLAD_GLAPI_EXPORT_BUILD)
+#    if defined(__GNUC__)
+#     define GLAPI __attribute__ ((dllexport)) extern
+#    else
+#     define GLAPI __declspec(dllexport) extern
+#    endif
+#   else
+#    if defined(__GNUC__)
+#     define GLAPI __attribute__ ((dllimport)) extern
+#    else
+#     define GLAPI __declspec(dllimport) extern
+#    endif
+#   endif
+#  elif defined(__GNUC__) && defined(GLAD_GLAPI_EXPORT_BUILD)
+#   define GLAPI __attribute__ ((visibility ("default"))) extern
+#  else
+#   define GLAPI extern
+#  endif
+# else
+#  define GLAPI extern
+# endif
 #endif
 
 GLAPI struct gladGLversionStruct GLVersion;
