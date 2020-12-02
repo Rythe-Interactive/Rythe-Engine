06cfe70dc978ebf00ed164d1277bb374be92960d
Arjen de Aldrey <adealdrey3@gmail.com>
2020-12-02 18:24:51 +0100

    [FIX] framebuffers work

--- legion/engine/rendering/data/renderbuffer.hpp
+++ legion/engine/rendering/data/renderbuffer.hpp
@@ -10,6 +10,7 @@ namespace legion::rendering
         renderbuffer(GLenum internalformat, math::ivec2 resolution, int samples = 0)
         {
             glGenRenderbuffers(1, &id);
+            bind();
             if (samples > 0)
                 glNamedRenderbufferStorageMultisample(id, samples, internalformat, resolution.x, resolution.y);
             else
@@ -19,6 +20,7 @@ namespace legion::rendering
         renderbuffer(GLenum internalformat, int width, int height, int samples = 0)
         {
             glGenRenderbuffers(1, &id);
+            bind();
             if (samples > 0)
                 glNamedRenderbufferStorageMultisample(id, samples, internalformat, width, height);
             else
