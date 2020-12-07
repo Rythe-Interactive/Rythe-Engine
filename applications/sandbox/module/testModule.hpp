07b791d99a3c117d684378c9764b309bf9a812ed
kukash <469260@student.saxion.l>
2020-12-07 15:46:19 +0100

    changed test system back to default test system in sandbox

--- applications/sandbox/module/testModule.hpp
+++ applications/sandbox/module/testModule.hpp
@@ -15,7 +15,7 @@ public:
     virtual void setup() override
     {
 
-        if (true)
+        if (false)
         {
             reportSystem<PointCloudTestSystem>();
         //    reportSystem<PointCloudGeneration>();
