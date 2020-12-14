fb5c13a539ffd8791e46f86b3edec715b088e00d
GlynLeine <g.m.leine@gmail.com>
2020-12-14 09:32:56 +0100

    comments on vertexarray

--- legion/engine/rendering/data/vertexarray.hpp
+++ legion/engine/rendering/data/vertexarray.hpp
@@ -2,58 +2,48 @@
 #include <application/application.hpp>
 #include <rendering/data/buffer.hpp>
 
+/**@file vertexarray.hpp
+ */
+
 namespace legion::rendering
 {
+    /**@class vertexarray
+     * @brief Low level vertexarray object.
+     * @note Read more at <a href="https://www.khronos.org/opengl/wiki/Vertex_Specification#Vertex_Array_Object">khronos.org wiki.</a>
+     */
     struct vertexarray
     {
     private:
+        // Managed resource with the VAO id, also has the responsibility of deleting the VAO after all copies of this renderbuffer have been destroyed.
         common::managed_resource<app::gl_id> m_id = common::managed_resource<app::gl_id>(nullptr);
 
     public:
+
+        /**@brief Faux constructor. To prevent unnecessary GPU allocations the default constructor doesn't actually create a VAO.
+         *        This means that default initialized vertexarrays are invalid temporary objects until they get properly initialized.
+         */
         vertexarray() = default;
 
-        explicit vertexarray(std::nullptr_t t) : m_id([](app::gl_id& value) { glDeleteVertexArrays(1, &value); }, invalid_id)
-        {
-            glGenVertexArrays(1, &m_id);
-        }
+        /**@brief Main allocating constructor. This constructor actually creates the GPU side renderbuffer.
+         * @param t temporary message value that tells the compiler to use the right constructor.
+         */
+        explicit vertexarray([[maybe_unused]] std::nullptr_t t);
 
-        static vertexarray generate()
-        {
-            return vertexarray(nullptr);
-        }
+        /**@brief Static function to generate a VAO. Alternative to the vertexarray(std::nullptr_t) constructor.
+         * @return vertexarray Newly created VAO.
+         */
+        static vertexarray generate();
 
-        bool setAttribPointer(const buffer& buf, uint index, size_type size, GLenum type, bool normalized, size_type stride, size_type offset)
-        {
-            if (buf.target() != GL_ARRAY_BUFFER)
-            {
-                log::error("Attempted to set attribute pointer for non GL_ARRAY_BUFFER, attribute pointers only work for VBOs. VAO: {} Buffer: {}", m_id.value, buf.id());
-                return false;
-            }
+        void setAttribPointer(const buffer& buf, uint index, size_type size, GLenum type, bool normalized, size_type stride, size_type offset);
 
-            glBindVertexArray(m_id);
-            buf.bind();
-            glEnableVertexAttribArray(index);
-            glVertexAttribPointer(index, size, type, normalized, stride, reinterpret_cast<GLvoid*>(offset));
-            buf.release();
-            glBindVertexArray(0);
-            return true;
-        }
+        void setAttribDivisor(uint index, uint divisor);
 
-        void setAttribDivisor(uint index, uint divisor)
-        {
-            glBindVertexArray(m_id);
-            glVertexAttribDivisor(index, divisor);
-            glBindVertexArray(0);
-        }
+        /**@brief Bind the vertexarray to the current context. Useful for low level native rendering.
+         */
+        void bind() const;
 
-        void bind() const
-        {
-            glBindVertexArray(m_id);
-        }
-
-        static void release()
-        {
-            glBindVertexArray(0);
-        }
+        /**@brief Release the vertexarray from the current context. Useful for low level native rendering.
+         */
+        static void release();
     };
 }
