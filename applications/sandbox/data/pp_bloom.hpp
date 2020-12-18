5b310f86c9aecfa405ae2ec14bb92447836a8d0b
GlynLeine <g.m.leine@gmail.com>
2020-12-18 15:40:49 +0100

    make pp bloom

--- applications/sandbox/data/pp_bloom.hpp
+++ applications/sandbox/data/pp_bloom.hpp
@@ -9,6 +9,7 @@ namespace legion::rendering
     private:
         shader_handle m_brightnessThresholdShader;
         shader_handle m_gaussianBlurShader;
+        shader_handle m_combineShader;
         //m_shader3
         texture_handle m_brightTexture;
         framebuffer m_pingpongFrameBuffers[2];
@@ -36,12 +37,13 @@ namespace legion::rendering
             //get shaders
             m_brightnessThresholdShader = rendering::ShaderCache::create_shader("brightnessthreshold_shader", "assets://shaders/brightnessthresholdshader.shs"_view);
             m_gaussianBlurShader = rendering::ShaderCache::create_shader("gaussianblur_shader", "assets://shaders/gaussianblurshader.shs"_view);
+            m_combineShader = rendering::ShaderCache::create_shader("m_combineShader", "assets://shaders/bloomshader.shs"_view);
 
-            for(int i = 0; i < 2;i++)
+            for (int i = 0; i < 2; i++)
             {
                 m_pingpongFrameBuffers[i] = framebuffer(GL_FRAMEBUFFER);
-                m_pingpongTextureBuffers[i] = rendering::TextureCache::create_texture("blurTexture"+ i, context.size(), settings);
-                m_pingpongFrameBuffers[i].attach(m_pingpongTextureBuffers[i],GL_COLOR_ATTACHMENT0);
+                m_pingpongTextureBuffers[i] = rendering::TextureCache::create_texture("blurTexture" + i, context.size(), settings);
+                m_pingpongFrameBuffers[i].attach(m_pingpongTextureBuffers[i], GL_COLOR_ATTACHMENT0);
             }
 
             addRenderPass<&PostProcessingBloom::renderPass>();
@@ -49,12 +51,14 @@ namespace legion::rendering
 
         void renderPass(framebuffer& fbo, texture_handle colortexture, texture_handle depthtexture)
         {
-            if(!m_brightTexture)
+            if (!m_brightTexture)
             {
                 m_brightTexture = TextureCache::create_texture("brightTexture", colortexture.get_texture().size(), settings);
             }
 
-            m_brightTexture.get_texture().resize(colortexture.get_texture().size());
+            math::ivec2 framebufferSize = colortexture.get_texture().size();
+            if (m_brightTexture.get_texture().size() != framebufferSize)
+                m_brightTexture.get_texture().resize(framebufferSize);
 
             fbo.attach(m_brightTexture, GL_COLOR_ATTACHMENT1);
 
@@ -79,15 +83,19 @@ namespace legion::rendering
             int amount = 10;
             m_gaussianBlurShader.bind();
 
-            m_pingpongTextureBuffers[0].get_texture().resize(colortexture.get_texture().size());
-            m_pingpongTextureBuffers[1].get_texture().resize(colortexture.get_texture().size());
+            if (m_pingpongTextureBuffers[0].get_texture().size() != framebufferSize)
+            {
+                m_pingpongTextureBuffers[0].get_texture().resize(framebufferSize);
+                m_pingpongTextureBuffers[1].get_texture().resize(framebufferSize);
+            }
 
-            for(uint i = 0; i < amount; i++)
+            for (uint i = 0; i < amount; i++)
             {
                 m_gaussianBlurShader.get_uniform<bool>("horizontal").set_value(horizontal);
 
                 if (first_iteration)
                 {
+                    first_iteration = false;
                     m_gaussianBlurShader.get_uniform<texture_handle>("image").set_value(m_brightTexture);
                 }
                 else
@@ -98,21 +106,19 @@ namespace legion::rendering
                 m_pingpongFrameBuffers[horizontal].bind();
                 renderQuad();
                 horizontal = !horizontal;
-                if (first_iteration)
-                    first_iteration = false;
             }
             m_pingpongFrameBuffers[horizontal].release();
             m_gaussianBlurShader.release();
-            
+
             //Add m_brightTexture to colorTexture
             //Render to GL_COLOR_ATTACHMENT0
 
             fbo.bind();
-            auto scrnshader = rendering::ShaderCache::get_handle("screen shader");
-            scrnshader.bind();
-            scrnshader.get_uniform<texture_handle>("screenTexture").set_value(m_pingpongTextureBuffers[horizontal]);
+            m_combineShader.bind();
+            m_combineShader.get_uniform<texture_handle>("screenTexture").set_value(colortexture);
+            m_combineShader.get_uniform<texture_handle>("brightTexture").set_value(m_pingpongTextureBuffers[!horizontal]);
             renderQuad();
-            scrnshader.release();
+            m_combineShader.release();
             fbo.release();
         }
     };
