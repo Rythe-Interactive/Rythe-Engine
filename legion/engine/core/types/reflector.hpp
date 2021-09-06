878b7d2272b3f100224cedc6aa6a373a096d5781
GlynLeine <g.m.leine@gmail.com>
2021-09-07 01:34:16 +0200

    fixed typo in decltypes(...), improved is_container, added is_resizable_container

--- legion/engine/core/types/reflector.hpp
+++ legion/engine/core/types/reflector.hpp
@@ -56,7 +56,7 @@ namespace legion::core{                                                         
         using source_type = type;                                                                                                                            \
                                                                                                                                                              \
         inline static constexpr std::size_t size = EXPAND(NARGS(__VA_ARGS__));                                                                               \
-        std::tuple<EXPAND(decltypes_count(NARGS(__VA_ARGS__), colon_access(type, __VA_ARGS__)))> values;                                                     \
+        std::tuple<decltypes(colon_access(type, __VA_ARGS__))> values;                                                                                       \
         std::array<std::string, size> names;                                                                                                                 \
                                                                                                                                                              \
         reflector(const type& src) : values(std::make_tuple(EXPAND(dot_access(src, __VA_ARGS__)))), names({ EXPAND(STRINGIFY_SEPERATE(__VA_ARGS__)) }) {}    \
