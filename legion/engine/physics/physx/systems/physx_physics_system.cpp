39181a7f4a3dbb4c1c4a8bd8038e80d8bb356719
Raphael Priatama <raphaelpriatama@gmail.com>
2022-02-24 10:50:22 +0700

    PxPhysics to static

--- legion/engine/physics/physx/systems/physx_physics_system.cpp
+++ legion/engine/physics/physx/systems/physx_physics_system.cpp
@@ -12,6 +12,7 @@ namespace legion::physics
         static PxFoundation* foundation;
         static PxPvd* pvd;
         static PxDefaultCpuDispatcher* dispatcher;
+        static PxPhysics* phyxSDK;
 
         static PxDefaultAllocator defaultAllocator;
         static PxDefaultErrorCallback defaultErrorCallback;
@@ -22,11 +23,11 @@ namespace legion::physics
     PxFoundation* physXStatics::foundation = nullptr;
     PxPvd* physXStatics::pvd = nullptr;
     PxDefaultCpuDispatcher* physXStatics::dispatcher = nullptr;
+    PxPhysics* physXStatics::phyxSDK = nullptr;
+
     PxDefaultAllocator physXStatics::defaultAllocator;
     PxDefaultErrorCallback physXStatics::defaultErrorCallback;
     
-    PxPhysics* gPhysics = nullptr;
-
     //PhysX PVD Debugger Related
     constexpr char* pvdHost = "127.0.0.1";
     constexpr size_type defaultPVDListeningPort = 5425;
@@ -66,29 +67,26 @@ namespace legion::physics
             physXStatics::pvd->connect(*transport, PxPvdInstrumentationFlag::eDEBUG);
 
             physXStatics::dispatcher = PxDefaultCpuDispatcherCreate(0); //deal with multithreading later on
-        }
 
-        if (!gPhysics)
-        {
-            gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *physXStatics::foundation, PxTolerancesScale(), true, physXStatics::pvd);
+            physXStatics::phyxSDK = PxCreatePhysics(PX_PHYSICS_VERSION, *physXStatics::foundation, PxTolerancesScale(), true, physXStatics::pvd);
         }
     }
 
     void PhysXPhysicsSystem::setupDefaultScene()
     {
-        PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+        PxSceneDesc sceneDesc(physXStatics::phyxSDK->getTolerancesScale());
         sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
         sceneDesc.cpuDispatcher = physXStatics::dispatcher;
         sceneDesc.filterShader = PxDefaultSimulationFilterShader;
         sceneDesc.flags |= PxSceneFlag::eENABLE_STABILIZATION;
 
-        m_physxScene = gPhysics->createScene(sceneDesc);
+        m_physxScene = physXStatics::phyxSDK->createScene(sceneDesc);
     }
 
     void PhysXPhysicsSystem::releasePhysXVariables()
     {
         physXStatics::dispatcher->release();
-        gPhysics->release();
+        physXStatics::phyxSDK->release();
 
         if (physXStatics::pvd)
         {
