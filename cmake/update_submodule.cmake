fa9834863eb8d08c76343251dfec627331845095
Leon Brands (Macbook) <lajbrands@outlook.com>
2022-03-02 21:04:51 +0100

    fixed a couple path issues

--- cmake/update_submodule.cmake
+++ cmake/update_submodule.cmake
@@ -2,12 +2,12 @@
 # Simply calls `git submodule update --init --recursive --rebase -- ${modulePath}`,
 # but only if the submodule hasn't already been updated (checks for common repo files).
 function(update_submodule modulePath)
-	if( NOT (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${modulePath}/README.md) AND
-		NOT (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${modulePath}/.gitignore) AND 
-		NOT (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/${modulePath}/CMakeLists.txt))
+	if( NOT (EXISTS ${RYTHE_DIR_ROOT}/${modulePath}/README.md) AND
+		NOT (EXISTS ${RYTHE_DIR_ROOT}/${modulePath}/.gitignore) AND 
+		NOT (EXISTS ${RYTHE_DIR_ROOT}/${modulePath}/CMakeLists.txt))
 		message(STATUS "Downloading ${modulePath}")
 		execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive --rebase -- ${modulePath}
-						WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+						WORKING_DIRECTORY ${RYTHE_DIR_ROOT}
 						RESULT_VARIABLE GIT_SUBMOD_RESULT)
 		if(NOT GIT_SUBMOD_RESULT EQUAL "0")
 			message(FATAL_ERROR "git submodule update failed with ${GIT_SUBMOD_RESULT}")
