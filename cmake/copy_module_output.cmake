e541af35e5f68efa5d9e0c68afe72a09810608bb
Leon Brands (Macbook) <lajbrands@outlook.com>
2022-03-03 21:32:11 +0100

    fixed copy_module_output

--- cmake/copy_module_output.cmake
+++ cmake/copy_module_output.cmake
@@ -1,10 +1,10 @@
-# Utility file that defines the post_build_copy() function.
-# post_build_copy adds post-build commands to targets that copy library and header files,
+# Utility file that defines the copy_module_output() function.
+# copy_module_output adds commands to targets that copy library and header files,
 # to the designated rythe include/ and lib/ folders.
 # Note: this utility function is **only** designed for use with rythe projects,
 # it uses ${RYTHE_DIR_ROOT} to get full filepaths and ${RYTHE_DIR_OUTPUT_INCLUDES} to copy results to.
 
-# Util function for internal use in post_build_copy.
+# Util function for internal use in copy_module_output.
 # Adds a post-build command to target that creates a directory at path p.
 function(create_dir target p)
 	add_custom_command(TARGET ${target} 
@@ -14,7 +14,7 @@ function(create_dir target p)
 	)
 endfunction()
 
-# Util function for internal use in post_build_copy.
+# Util function for internal use in copy_module_output.
 # Adds a post-build command to target that deletes a directory at path p.
 function(delete_dir target p)
 	add_custom_command(TARGET ${target} 
@@ -25,7 +25,7 @@ function(delete_dir target p)
 endfunction()
 
 # Add post-build commands to copy library and header files to output directories.
-function(post_build_copy targetName targetDir)
+function(copy_module_output targetName targetDir)
 	# Prepare directories for library output
 	create_dir(${targetName} ${RYTHE_DIR_OUTPUT_LIBS}/Debug)
 	create_dir(${targetName} ${RYTHE_DIR_OUTPUT_LIBS}/Release)
@@ -55,22 +55,26 @@ function(post_build_copy targetName targetDir)
 	# This is necessary because cmake copy takes a directory,
 	# and we want to make sure to copy the files to the correct directory.
 	macro(read_directory curdir)
+		set(SOURCE_PATH ${RYTHE_DIR_ROOT}/${targetDir}/src/${targetName}/${curdir})
+		set(DEST_PATH ${RYTHE_DIR_OUTPUT_INCLUDES}/${targetDir}/${curdir})
+
 		# Get all files and folders in the current directory
-		file(GLOB children RELATIVE ${RYTHE_DIR_ROOT}/${curdir}/ ${RYTHE_DIR_ROOT}/${curdir}/*)
+		file(GLOB children RELATIVE ${SOURCE_PATH} ${SOURCE_PATH}/*)
 		
 		# Clear files list
 		set(files "")
 
 		foreach(child ${children})
 			# Files can be added to our files list for copying
-			if (NOT (IS_DIRECTORY ${RYTHE_DIR_ROOT}/${curdir}/${child}))
+			if (NOT (IS_DIRECTORY ${SOURCE_PATH}/${child}))
 				get_filename_component(CHILD_EXT ${child} EXT)
 
 				# Only copy headers/inl files
 				if (CHILD_EXT STREQUAL ".hpp" OR 
 					CHILD_EXT STREQUAL ".h" OR 
 					CHILD_EXT STREQUAL ".inl")
-					list(APPEND files ${RYTHE_DIR_ROOT}/${curdir}/${child})
+
+					configure_file(${SOURCE_PATH}/${child} ${DEST_PATH}/${child} COPYONLY)
 				endif()
 			# Folders can be recursively looked through for more headers
 			else()
@@ -78,6 +82,7 @@ function(post_build_copy targetName targetDir)
 				if (${child} STREQUAL "Debug" OR 
 					${child} STREQUAL "Release" OR
 					${child} STREQUAL "CMakeFiles" OR 
+					${child} STREQUAL "CMakeScripts" OR 
 					${child} STREQUAL "${targetName}.dir")
 					continue()
 				endif()
@@ -89,17 +94,8 @@ function(post_build_copy targetName targetDir)
 
 		# Prepare the output directory for copying
 		create_dir(${targetName} ${RYTHE_DIR_OUTPUT_INCLUDES}/${curdir})
-
-		# Add post-build command that copies the selected files to the dedicated output folder
-		add_custom_command(TARGET ${targetName}
-			POST_BUILD
-			COMMAND ${CMAKE_COMMAND} -E 
-			copy
-				${files}
-				${RYTHE_DIR_OUTPUT_INCLUDES}/${curdir}/
-		)
 	endmacro()
 
 	# Run the macro on the root folder
-	read_directory(${targetDir})
+	read_directory("")
 endfunction()
