3a81ca0add29fc018a2727f1e48785529c2ee239
Glyn Leine <g.m.leine@gmail.com>
2022-12-27 18:49:26 +0100

    fix configure util target

--- VisualStudio22-All.bat
+++ VisualStudio22-All.bat
@@ -1,3 +1,2 @@
 cmake -E make_directory build
 cmake . -G "Visual Studio 17 2022" -S . -B ./build/ -T ClangCL -D RYTHE_BUILD_APPLICATIONS=ON -D RYTHE_BUILD_OPTION_ASAN=ON -D RYTHE_FORCE_ENABLE_ALL_MODULES=ON
-pause
