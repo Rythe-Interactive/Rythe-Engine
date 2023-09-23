f365a40ad4c89a0bd96a3c521e824ac6d822d963
Glyn Leine <g.m.leine@gmail.com>
2023-09-23 02:08:09 +0200

    update VisualStudio22-All.bat & core & physics

--- VisualStudio22-All.bat
+++ VisualStudio22-All.bat
@@ -1,2 +1,1 @@
-cmake -E make_directory build
-cmake . -G "Visual Studio 17 2022" -S . -B ./build/ -T ClangCL -D RYTHE_BUILD_APPLICATIONS=ON -D RYTHE_BUILD_OPTION_ASAN=OFF -D RYTHE_FORCE_ENABLE_ALL_MODULES=ON
+tools\premake5 vs2022
