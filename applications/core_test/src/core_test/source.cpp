6c026f86140ce28a77ef82bafc0857ec7e372edd
Glyn Leine <g.m.leine@gmail.com>
2025-08-23 00:55:04 +0200

    update rbs and rsl and .clang_format

--- applications/core_test/src/core_test/source.cpp
+++ applications/core_test/src/core_test/source.cpp
@@ -6,15 +6,15 @@ using namespace rythe::core;
 
 struct test_struct
 {
-	int i = 14;
+    int i = 14;
 };
 
 rsl::result<void> RYTHE_CCONV init_program(program& program)
 {
-	auto& engine = program.add_engine_instance();
-	engine.get_context().emplace<test_struct>().i = 15;
+    auto& engine = program.add_engine_instance();
+    engine.get_context().emplace<test_struct>().i = 15;
 
-	rsl::log::debug("{}", engine.get_context().get<test_struct>().i);
+    rsl::log::debug("{}", engine.get_context().get<test_struct>().i);
 
-	return rsl::error; // Temporarily to stop the engine from running.
+    return rsl::error; // Temporarily to stop the engine from running.
 }
--- applications/sandbox/src/sandbox/source.cpp
+++ applications/sandbox/src/sandbox/source.cpp
@@ -2,38 +2,40 @@
 #define RYTHE_LOG_DEBUG
 
 #if defined(NDEBUG)
-	#define RYTHE_KEEP_CONSOLE
+#define RYTHE_KEEP_CONSOLE
 #endif
 
 #include <core/core.hpp>
 #include <rsl/type_traits>
 
 template <typename T>
-struct foo
-{
-};
+struct foo {};
 
 rsl::result<void> RYTHE_CCONV init_program(rythe::core::program& program)
 {
-	using namespace rythe;
-	program.add_engine_instance();
+    using namespace rythe;
+    program.add_engine_instance();
 
-	[[maybe_unused]] constexpr rsl::constexpr_string A = "Something";
-	[[maybe_unused]] constexpr rsl::constexpr_string B = "Other";
-	[[maybe_unused]] constexpr rsl::constexpr_string result = A + B;
+    [[maybe_unused]] constexpr rsl::constexpr_string A = "Something";
+    [[maybe_unused]] constexpr rsl::constexpr_string B = "Other";
+    [[maybe_unused]] constexpr rsl::constexpr_string result = A + B;
 
-	[[maybe_unused]] constexpr rsl::constexpr_string hello_world = "hello world";
-	[[maybe_unused]] constexpr rsl::constexpr_string shorten = hello_world.filter_if([](char y) { return ' ' != y; });
-	[[maybe_unused]] constexpr rsl::constexpr_string optimal = shorten.refit<shorten.size() + 1>();
+    [[maybe_unused]] constexpr rsl::constexpr_string hello_world = "hello world";
+    [[maybe_unused]] constexpr rsl::constexpr_string shorten = hello_world.filter_if([](char y) { return ' ' != y; });
+    [[maybe_unused]] constexpr rsl::constexpr_string optimal = shorten.refit<shorten.size() + 1>();
 
-	constexpr rsl::constexpr_string typeName = rsl::type_name<std::string>();
-	[[maybe_unused]] constexpr auto shrunk = typeName.refit<typeName.size() + 1>();
-	constexpr rsl::id_type typeHash = rsl::type_id<std::string>();
+    constexpr rsl::constexpr_string typeName = rsl::type_name<std::string>();
+    [[maybe_unused]] constexpr auto shrunk = typeName.refit<typeName.size() + 1>();
+    constexpr rsl::id_type typeHash = rsl::type_id<std::string>();
 
-	rsl::log::debug(
-		"type info: {} : {}, {}, {}, {}", rsl::string_view(typeName), typeHash, typeName.size(), typeName.capacity(),
-		shrunk.capacity()
-	);
+    rsl::log::debug(
+            "type info: {} : {}, {}, {}, {}",
+            rsl::string_view(typeName),
+            typeHash,
+            typeName.size(),
+            typeName.capacity(),
+            shrunk.capacity()
+            );
 
-	return rsl::okay;
+    return rsl::okay;
 }
